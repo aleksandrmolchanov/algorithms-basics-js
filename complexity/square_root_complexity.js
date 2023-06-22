640eb1931e9aee2178f58b3200a8c64096b67313
Aleksandr Molchanov <alexandr87m@gmail.com>
2023-06-22 22:36:30 +0500

    fear: Add square root complexity example

---
+++ complexity/square_root_complexity.js
@@ -0,0 +1,14 @@
+const isPrime = (n) => {
+    for (let i = 2; i <= Math.sqrt(n); i++) {
+        if(n % i == 0) {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+console.log(isPrime(3));
+console.log(isPrime(4));
+console.log(isPrime(7));
+console.log(isPrime(9));
