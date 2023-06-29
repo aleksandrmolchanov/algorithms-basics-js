14e8575d6ca18b69eff7c546f3b0fde4fb36a202
Aleksandr Molchanov <alexandr87m@gmail.com>
2023-06-29 23:22:15 +0500

    feat(linked lists): Add LinkedList methods

--- linked_list/classes.js
+++ linked_list/classes.js
@@ -6,5 +6,76 @@ class LinkedListNode {
 }
 
 class LinkedList {
-    head = null
+    head = null;
+
+    add (value) {
+        this.head = new LinkedListNode(value, this.head);
+    }
+
+    insert (index, value) {
+        if (this.head === null || index === 0) {
+            this.add(value)
+        } else {
+            let current = this.head;
+            while (current.next !== null && index - 1 > 0) {
+                current = current.next;
+                index = index - 1;
+            }
+            current.next = new LinkedListNode(value, current.next);
+        }
+    }
+
+    contains (value) {
+        let current = this.head;
+        while(current !== null) {
+            if (current.value === value) {
+                return true;
+            }
+            current = current.next;
+        }
+        return false;
+    }
+
+    length () {
+        let result = 0;
+        let current = this.head;
+
+        while(current !== null) {
+            result = result + 1;
+            current = current.next;
+        }
+
+        return result;
+    }
+
+    remove () {
+        if (this.head === null) {
+            return undefined;
+        }
+
+        const value = this.head.value;
+        this.head = this.head.next;
+
+        return value;
+    }
+
+    removeAt (index) {
+        if (this.head === null) {
+            return undefined;
+        } else if (index === 0 || this.head.next === null) {
+            return this.remove();
+        } else {
+            let current = this.head;
+            while (current.next.next !== null && index - 1 > 0) {
+                current = current.next;
+                index = index - 1;
+            }
+            const value = current.next.value;
+            current.next = current.next.next;
+
+            return value;
+        }
+    }
 }
+
+
