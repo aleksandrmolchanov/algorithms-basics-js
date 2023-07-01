6930c1e9cf9e6721c89f59ffe2576e1fbc46b9d1
Aleksandr Molchanov <alexandr87m@gmail.com>
2023-07-01 19:44:37 +0500

    feat(doubly linked lists): Add classes

---
+++ doubly_linked_list/classes.js
@@ -0,0 +1,12 @@
+class DoublyLikedListNode {
+    constuctor(value, previous, next) {
+        this.value = value;
+        this.previous = previous;
+        this.next = next;
+    }
+}
+
+class DoublyLinkedList {
+    head = null;
+    tail = null;
+}
