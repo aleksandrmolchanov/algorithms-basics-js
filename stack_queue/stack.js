130c1dc383e09eb0be5b07b74f9a7f7ae864bca8
Aleksandr Molchanov <alexandr87m@gmail.com>
2023-07-04 23:07:32 +0500

    feat(stack): Add expression calculation example

--- stack_queue/stack.js
+++ stack_queue/stack.js
@@ -13,3 +13,37 @@ class Stack {
         return this.items.length === 0;
     }
 }
+
+let stack = new Stack();
+const expression = '3 2 + 4 5 + *';
+const lexems = expression.split(' ');
+for (let lexem of lexems) {
+    let a;
+    let b;
+    switch (lexem) {
+        case '+':
+            b = stack.pop();
+            a = stack.pop();
+            stack.push(a + b);
+            break;
+        case '-':
+            b = stack.pop();
+            a = stack.pop();
+            stack.push(a - b);
+            break;
+        case '*':
+            b = stack.pop();
+            a = stack.pop();
+            stack.push(a * b);
+            break;
+        case '/':
+            b = stack.pop();
+            a = stack.pop();
+            stack.push(a / b);
+            break;
+        default:
+            stack.push(parseFloat(lexem));
+    }
+}
+
+console.log(stack.pop());
