2831c95e36706e8eb48e172744d0fcdbcbef416a
Aleksandr Molchanov <alexandr87m@gmail.com>
2023-07-05 21:15:02 +0500

    feat(hash): Add sparse array example

---
+++ hash/sparse_array.js
@@ -0,0 +1,18 @@
+let capitals = [];
+
+capitals[881] = 'Vienna';
+capitals[1237] = 'Berlin';
+capitals[1323] = 'Vilnius';
+capitals[1614] = 'Tirana';
+capitals[1167] = 'Copenhagen';
+capitals[963] = 'Luxembourg';
+capitals[1067] = 'Minsk';
+capitals[1191] = 'Dublin';
+capitals[1275] = 'Amsterdam';
+capitals[752] = 'Vatican';
+capitals[1786] = 'Reykjavík';
+capitals[1200] = 'Warsaw';
+capitals[1872] = 'Budapest';
+capitals[856] = 'Madrid';
+capitals[1147] = 'Moscow';
+
